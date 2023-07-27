e21c20fcbf3f25713f9ac331c1fcb6cbb627c53e
MYX <it.anton22@gmail.com>
2023-07-27 19:53:16 +0300

    cmpleted third task, fineshed all tasks

---
+++ event-object/tabs/task.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const tab = document.querySelectorAll(".tab"); // выбираем элементы (вкладки) из DOM
+const tabsContainer = document.querySelector(".tabs"); // контейнер вкладок
+const tabContent = document.querySelectorAll(".tab__content"); // получаем контент вкладок из DOM
+
+// Функция, которая выполнится по клику на вкладку
+
+function tabClick(event) {
+  const clickedTab = event.target; // Получаем элемент (вкладку), на которую кликнули
+
+  // Проверяем, если текущая вкладка уже активна, то не делаем ничего
+  if (clickedTab.classList.contains("tab_active")) {
+    return;
+  }
+
+  // Удаляем класс "tab_active" у всех вкладок
+  tab.forEach((tabItem) => {
+    tabItem.classList.remove("tab_active");
+  });
+
+  // Добавляем класс "tab_active" только на текущую вкладку
+  clickedTab.classList.add("tab_active");
+
+  // Находим индекс текущей вкладки среди всех вкладок
+  const index = Array.from(tab).indexOf(clickedTab);
+
+  // Отображаем контент соответствующей вкладки
+  tabContent.forEach((contentItem) => {
+    contentItem.classList.remove("tab__content_active");
+  });
+
+  tabContent[index].classList.add("tab__content_active");
+}
+
+// Добавляем обработчик события "click" на контейнер вкладок
+tabsContainer.addEventListener("click", tabClick);
