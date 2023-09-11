b95a6ccaee27db720e134f889586cc38b3d0dfc0
MYX <it.anton22@gmail.com>
2023-09-11 22:06:57 +0300

    comleted first task

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -2,58 +2,48 @@
 
 const signinForm = document.getElementById('signin__form');
 const welcome = document.getElementById('welcome');
-let userId;
 
-signinForm.onsubmit = async (e) => {//fetch
+async function authorizeUser(formData) {// Функция для выполнения авторизации
+    try {
+        const response = await fetch('https://students.netoservices.ru/nestjs-backend/auth', {
+            method: 'POST',
+            body: formData
+        });
+
+        const result = await response.json();
+
+        if (result.success) {
+            // Сохраняем id пользователя в локальное хранилище
+            localStorage.setItem('id', result.user_id);
+            // Отображаем id пользователя в блоке welcome
+            welcome.textContent = `Добро пожаловать, ${result.user_id}!`;
+            welcome.classList.add('welcome_active');
+            
+            document.getElementById('signin').classList.remove('signin_active');
+        } else {
+            // Выводим сообщение об ошибке
+            alert('Неверный логин/пароль');
+        }
+    } catch (error) {
+        console.error('Ошибка при отправке запроса:', error);
+    }
+}
+
+// Проверяем наличие id пользователя в локальном хранилище при загрузке страницы
+window.addEventListener('load', () => {
+    const storedUserId = localStorage.getItem('id');
+    if (storedUserId) {
+        // Отображаем блок welcome с сохраненным id пользователя
+        welcome.textContent = `Добро пожаловать, ${storedUserId}!`;
+        welcome.classList.add('welcome_active');
+        document.getElementById('signin').classList.remove('signin_active');
+    }
+});
+
+// Обработчик события отправки формы
+signinForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const formData = new FormData(signinForm);
-
-    const response = await fetch('https://students.netoservices.ru/nestjs-backend/auth', {
-        method: 'POST',
-        body: formData
-    });
-
-    const result = await response.json();
-    if (result.success) {
-        document.getElementById('signin').classList.remove('signin_active');
-        userId = result.user_id;
-        setCookie('id', userId, { 'expires': new Date('2038-01-19T03:14:07.000Z') });
-        welcome.textContent = `Добро пожаловать, пользователь #${userId}`;
-        welcome.classList.add('welcome_active');
-        console.log(getCookie('id'));
-    } else {
-        alert('Неверный логин/пароль');
-    }
-};
-
-function setCookie(name, value, options = {}) {//готовая форма из книжки learninf js
-    options = {
-        path: '/',
-        // при необходимости добавьте другие значения по умолчанию
-        ...options
-    };
-
-    if (options.expires instanceof Date) {
-        options.expires = options.expires.toUTCString();
-    }
-
-    let updatedCookie = `${name}=${value}`;
-
-    for (let optionKey in options) {
-        updatedCookie += `; ${optionKey}`;
-        let optionValue = options[optionKey];
-        if (optionValue !== true) {
-            updatedCookie += `=${optionValue}`;
-        }
-    }
-
-    document.cookie = updatedCookie;
-}
-
-function getCookie(name) {//готовая форма из книжки learninf js
-    let matches = document.cookie.match(new RegExp(
-        `(?:^|; )${name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1')}=([^;]*)`
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-}
+    await authorizeUser(formData); // Вызываем функцию для авторизации
+});
